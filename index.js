22747b4c9b18b3219e3de5647c949a977b6befc8
Jonatan Morales <johnta2610@hotmail.com>
2023-04-21 09:40:09 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+/*
+    Dev Containers: Add...
+    Javascript & MongoDB
+    Version 18
+
+    Crear un proyecto de JS en Node
+
+        Package.json Personalizado
+        npm init -> dwpc1-2023a -> 1.0.0 -> Proyecto de aprendizaje de la materia de Pila Completa 1
+        index.js ->  -> #repositorio -> node, express, itgam -> @johnmorales -> MIT 
+
+        Crear una nueva llave dentro del Package.json
+        "type": "module",
+        "engines": {
+            "node": "18.x",
+            "npm": "9.x"
+        },
+
+        "scripts": {
+            "start": "node ./"
+        }
+
+        <------------------------------------------------------------------------------------------------------------->
+
+        Package.json Rapido
+        npm init -y
+*/
