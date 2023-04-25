b79c92a9b8aacad9726d4fdf4a543e5cdb950517
Jonatan Morales <johnta2610@hotmail.com>
2023-04-25 08:26:26 -0600

    Recreando Built-In Variables

--- index.js
+++ index.js
@@ -27,3 +27,29 @@
         Package.json Rapido
         npm init -y
 */
+
+//  1. Importando el modulo de Node HTTP
+import http from 'http';
+import path from 'path';
+// --> let http = require('http');
+
+//  Recreando Built-In Variables
+global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
+global["__filename"] = path.join(__dirname, path.basename(new URL(import.meta.url).pathname));
+
+//  2. Crear el servidor con toda la logica
+const server = http.createServer((request, response) => {
+    //  Toda la logica del servidor
+    //  1. Respondiendo al cliente
+    response.write(`
+    __dirname: ${__dirname}
+    __filename: ${__filename}
+    `);
+    //  2. Cerrar la conexion
+    response.end();
+});
+
+//  3. Encender el servidor
+server.listen(3000, "0.0.0.0", () => {
+    console.log("Servidor escuchando en http://localhost:3000");
+});
